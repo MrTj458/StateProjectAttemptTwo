5198da7da6bb1c01dd54d0705f779ab116f57873
Trevor <thod0127@740s-pl-131.local>
2015-10-14 13:21:37 -0600

    Added to the repository!

---
+++ app/src/main/java/ctec/stateprojectattempttwo/controller/StateActivity.java
@@ -0,0 +1,43 @@
+package ctec.stateprojectattempttwo.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.CheckBox;
+import android.view.View;
+import android.content.Intent;
+
+public class StateActivity extends Activity
+{
+    private Button nextScreenButton;
+    private EditText nameText;
+    private EditText ageText;
+    private CheckBox isTiredBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_state);
+
+        nextScreenButton = (Button) findViewById(R.id.SubmitButton);
+        nameText = (EditText) findViewById(R.id.nameBox);
+        ageText = (EditText) findViewById(R.id.ageBox);
+        isTiredBox = (CheckBox) findViewById(R.id.isTiredBox);
+
+        setupListeners();
+    }
+
+    private void setupListeners()
+    {
+        nextScreenButton.setOnClickListener(new View.OnClickListener()
+        {
+           public void onClick(View clickView)
+           {
+               Intent changeScreen = new Intent(clickView.getContext(), BackPageActivity.class);
+               startActivityForResult(changeScreen, 0);
+           }
+        });
+    }
+}
