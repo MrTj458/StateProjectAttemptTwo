c71aea4b6f5af993f7550ad9739812177a29ac07
Trevor <thod0127@740s-pl-131.local>
2015-10-14 13:55:58 -0600

    Added listeners

--- app/src/main/java/ctec/stateprojectattempttwo/controller/BackPageActivity.java
+++ app/src/main/java/ctec/stateprojectattempttwo/controller/BackPageActivity.java
@@ -1,13 +1,38 @@
 package ctec.stateprojectattempttwo.controller;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
-public class BackPageActivity extends AppCompatActivity {
+public class BackPageActivity extends Activity
+{
+    private TextView userText;
+    private Button backButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_back_page);
+
+        userText = (TextView) findViewById(R.id.userText);
+        backButton = (Button) findViewById(R.id.backButton);
+
+        setupListeners();
+    }
+
+    private void setupListeners()
+    {
+        backButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View clickView)
+            {
+                Intent returnIntent = new Intent();
+
+            }
+        });
     }
 }
